bd0b1e9562fb2097aaf15a194d9f4bc4f975037c
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-11-06 00:34:07 +0600

    left side data added of our process

--- src/app/home/OurProcess.jsx
+++ src/app/home/OurProcess.jsx
@@ -7,42 +7,167 @@ const OurProcess = () => {
         <h1 className="text-5xl font-bold text-center text-white">
           Our process is simple, smooth <br /> and efficient.
         </h1>
-        <div className="grid lg:grid-cols-2 gap-10 mx-5 lg:mx-10">
+        <div className="grid lg:grid-cols-2 gap-10 mx-5 lg:mx-10 my-16">
           <div>
-            <div className="flex gap-5">
-              <div className="bg-red-200 w-12 h-12 rounded-full flex justify-center items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                >
-                  <g id="vuesax/linear/call">
-                    <g id="call">
-                      <g id="call_2">
-                        <path
-                          id="Vector"
-                          d="M18.3084 15.2751C18.3084 15.5751 18.2417 15.8834 18.1001 16.1834C17.9584 16.4834 17.7751 16.7667 17.5334 17.0334C17.1251 17.4834 16.6751 17.8084 16.1667 18.0167C15.6667 18.2251 15.1251 18.3334 14.5417 18.3334C13.6917 18.3334 12.7834 18.1334 11.8251 17.7251C10.8667 17.3167 9.90842 16.7667 8.95842 16.0751C8.00008 15.3751 7.09175 14.6001 6.22508 13.7417C5.36675 12.8751 4.59175 11.9667 3.90008 11.0167C3.21675 10.0667 2.66675 9.11675 2.26675 8.17508C1.86675 7.22508 1.66675 6.31675 1.66675 5.45008C1.66675 4.88341 1.76675 4.34175 1.96675 3.84175C2.16675 3.33341 2.48341 2.86675 2.92508 2.45008C3.45841 1.92508 4.04175 1.66675 4.65841 1.66675C4.89175 1.66675 5.12508 1.71675 5.33341 1.81675C5.55008 1.91675 5.74175 2.06675 5.89175 2.28341L7.82508 5.00842C7.97508 5.21675 8.08341 5.40841 8.15841 5.59175C8.23341 5.76675 8.27508 5.94175 8.27508 6.10008C8.27508 6.30008 8.21675 6.50008 8.10008 6.69175C7.99175 6.88341 7.83341 7.08341 7.63341 7.28341L7.00008 7.94175C6.90841 8.03341 6.86675 8.14175 6.86675 8.27508C6.86675 8.34175 6.87508 8.40008 6.89175 8.46675C6.91675 8.53341 6.94175 8.58341 6.95842 8.63341C7.10842 8.90841 7.36675 9.26675 7.73341 9.70008C8.10841 10.1334 8.50841 10.5751 8.94175 11.0167C9.39175 11.4584 9.82508 11.8667 10.2667 12.2417C10.7001 12.6084 11.0584 12.8584 11.3417 13.0084C11.3834 13.0251 11.4334 13.0501 11.4917 13.0751C11.5584 13.1001 11.6251 13.1084 11.7001 13.1084C11.8417 13.1084 11.9501 13.0584 12.0417 12.9667L12.6751 12.3417C12.8834 12.1334 13.0834 11.9751 13.2751 11.8751C13.4667 11.7584 13.6584 11.7001 13.8667 11.7001C14.0251 11.7001 14.1917 11.7334 14.3751 11.8084C14.5584 11.8834 14.7501 11.9917 14.9584 12.1334L17.7167 14.0917C17.9334 14.2417 18.0834 14.4167 18.1751 14.6251C18.2584 14.8334 18.3084 15.0417 18.3084 15.2751Z"
-                          stroke="#0C0A25"
-                          strokeMiterlimit="10"
-                        ></path>
+            <div className="mb-16">
+              <div className="flex gap-5 items-end">
+                <div className="bg-red-200 w-12 h-12 rounded-full flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <g id="vuesax/linear/call">
+                      <g id="call">
+                        <g id="call_2">
+                          <path
+                            id="Vector"
+                            d="M18.3084 15.2751C18.3084 15.5751 18.2417 15.8834 18.1001 16.1834C17.9584 16.4834 17.7751 16.7667 17.5334 17.0334C17.1251 17.4834 16.6751 17.8084 16.1667 18.0167C15.6667 18.2251 15.1251 18.3334 14.5417 18.3334C13.6917 18.3334 12.7834 18.1334 11.8251 17.7251C10.8667 17.3167 9.90842 16.7667 8.95842 16.0751C8.00008 15.3751 7.09175 14.6001 6.22508 13.7417C5.36675 12.8751 4.59175 11.9667 3.90008 11.0167C3.21675 10.0667 2.66675 9.11675 2.26675 8.17508C1.86675 7.22508 1.66675 6.31675 1.66675 5.45008C1.66675 4.88341 1.76675 4.34175 1.96675 3.84175C2.16675 3.33341 2.48341 2.86675 2.92508 2.45008C3.45841 1.92508 4.04175 1.66675 4.65841 1.66675C4.89175 1.66675 5.12508 1.71675 5.33341 1.81675C5.55008 1.91675 5.74175 2.06675 5.89175 2.28341L7.82508 5.00842C7.97508 5.21675 8.08341 5.40841 8.15841 5.59175C8.23341 5.76675 8.27508 5.94175 8.27508 6.10008C8.27508 6.30008 8.21675 6.50008 8.10008 6.69175C7.99175 6.88341 7.83341 7.08341 7.63341 7.28341L7.00008 7.94175C6.90841 8.03341 6.86675 8.14175 6.86675 8.27508C6.86675 8.34175 6.87508 8.40008 6.89175 8.46675C6.91675 8.53341 6.94175 8.58341 6.95842 8.63341C7.10842 8.90841 7.36675 9.26675 7.73341 9.70008C8.10841 10.1334 8.50841 10.5751 8.94175 11.0167C9.39175 11.4584 9.82508 11.8667 10.2667 12.2417C10.7001 12.6084 11.0584 12.8584 11.3417 13.0084C11.3834 13.0251 11.4334 13.0501 11.4917 13.0751C11.5584 13.1001 11.6251 13.1084 11.7001 13.1084C11.8417 13.1084 11.9501 13.0584 12.0417 12.9667L12.6751 12.3417C12.8834 12.1334 13.0834 11.9751 13.2751 11.8751C13.4667 11.7584 13.6584 11.7001 13.8667 11.7001C14.0251 11.7001 14.1917 11.7334 14.3751 11.8084C14.5584 11.8834 14.7501 11.9917 14.9584 12.1334L17.7167 14.0917C17.9334 14.2417 18.0834 14.4167 18.1751 14.6251C18.2584 14.8334 18.3084 15.0417 18.3084 15.2751Z"
+                            stroke="#0C0A25"
+                            strokeMiterlimit="10"
+                          ></path>
+                        </g>
                       </g>
                     </g>
-                  </g>
-                </svg>
-              </div>
-              <div>
+                  </svg>
+                </div>
+
                 <h1 className="text-2xl text-white">
                   Initiate a Strategy Session.
                 </h1>
-                <h1 className="text-gray-200">
-                  Engage with us in a consultative meeting to explore your
-                  business goals. We&apos;ll craft a tailored approach, define
-                  key objectives, establish timelines, and determine necessary
-                  resources.
+              </div>
+              <p className="text-gray-200 my-5 px-16">
+                Engage with us in a consultative meeting to explore your
+                business goals. We&apos;ll craft a tailored approach, define key
+                objectives, establish timelines, and determine necessary
+                resources.
+              </p>
+            </div>
+            <div className="mb-16">
+              <div className="flex gap-5 items-end">
+                <div className="bg-green-200 w-12 h-12 rounded-full flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <g id="vuesax/linear/people">
+                      <g id="people">
+                        <g id="Group">
+                          <path
+                            id="Vector"
+                            d="M14.9999 5.96675C14.9499 5.95842 14.8916 5.95842 14.8416 5.96675C13.6916 5.92508 12.7749 4.98342 12.7749 3.81675C12.7749 2.62508 13.7332 1.66675 14.9249 1.66675C16.1166 1.66675 17.0749 2.63342 17.0749 3.81675C17.0666 4.98342 16.1499 5.92508 14.9999 5.96675Z"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                          <path
+                            id="Vector_2"
+                            d="M14.1415 12.0333C15.2831 12.225 16.5415 12.025 17.4248 11.4333C18.5998 10.65 18.5998 9.36664 17.4248 8.58331C16.5331 7.99164 15.2581 7.79163 14.1165 7.99163"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                        </g>
+                        <g id="Group_2">
+                          <path
+                            id="Vector_3"
+                            d="M4.9749 5.96675C5.0249 5.95842 5.08324 5.95842 5.13324 5.96675C6.28324 5.92508 7.1999 4.98342 7.1999 3.81675C7.1999 2.62508 6.24157 1.66675 5.0499 1.66675C3.85824 1.66675 2.8999 2.63342 2.8999 3.81675C2.90824 4.98342 3.8249 5.92508 4.9749 5.96675Z"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                          <path
+                            id="Vector_4"
+                            d="M5.83328 12.0333C4.69162 12.225 3.43328 12.025 2.54995 11.4333C1.37495 10.65 1.37495 9.36664 2.54995 8.58331C3.44162 7.99164 4.71662 7.79163 5.85828 7.99163"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                        </g>
+                        <g id="Group_3">
+                          <path
+                            id="Vector_5"
+                            d="M10.0001 12.1916C9.95015 12.1833 9.89181 12.1833 9.84181 12.1916C8.69181 12.1499 7.77515 11.2083 7.77515 10.0416C7.77515 8.84994 8.73348 7.8916 9.92514 7.8916C11.1168 7.8916 12.0751 8.85827 12.0751 10.0416C12.0668 11.2083 11.1501 12.1583 10.0001 12.1916Z"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                          <path
+                            id="Vector_6"
+                            d="M7.5751 14.8166C6.4001 15.5999 6.4001 16.8832 7.5751 17.6666C8.90843 18.5582 11.0918 18.5582 12.4251 17.6666C13.6001 16.8832 13.6001 15.5999 12.4251 14.8166C11.1001 13.9332 8.90843 13.9332 7.5751 14.8166Z"
+                            stroke="#0C0A25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+
+                <h1 className="text-2xl text-white">Shape Your Dream Team.</h1>
+              </div>
+              <p className="text-gray-200 my-5 px-16">
+                Quickly solidify your project&apos;s blueprints, decide on a
+                collaborative model, and seamlessly integrate our hand-picked
+                experts with your vision.
+              </p>
+            </div>
+            <div className="mb-16">
+              <div className="flex gap-5 items-end">
+                <div className="bg-blue-200 w-12 h-12 rounded-full flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <g id="vuesax/linear/login">
+                      <g id="login">
+                        <path
+                          id="Vector"
+                          d="M9.7334 12.1834L11.8667 10.0501L9.7334 7.91675"
+                          stroke="#0C0A25"
+                          strokeMiterlimit="10"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>
+                        <path
+                          id="Vector_2"
+                          d="M3.33325 10.05H11.8083"
+                          stroke="#0C0A25"
+                          strokeMiterlimit="10"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>
+                        <path
+                          id="Vector_3"
+                          d="M10 3.33325C13.6833 3.33325 16.6667 5.83325 16.6667 9.99992C16.6667 14.1666 13.6833 16.6666 10 16.6666"
+                          stroke="#0C0A25"
+                          strokeMiterlimit="10"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+
+                <h1 className="text-2xl text-white">
+                  Begin Your Journey with Optimism.
                 </h1>
               </div>
+              <p className="text-gray-200 my-5 px-16">
+                Agree on pivotal goals, and we’ll get the ball rolling. Expect
+                proactive updates as we align our progress with your evolving
+                needs. Your satisfaction is our cue to excel.
+              </p>
             </div>
           </div>
 
